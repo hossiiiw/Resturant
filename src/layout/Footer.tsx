f0d74aaeb9b02b31f9420bfd9050865d739898a1
hossiiiw <hossiiw.0933@gmail.com>
2025-07-31 11:46:03 +0330

    responsive footer

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -9,17 +9,17 @@ function Footer() {
   return (
     <div className="w-full flex flex-col items-center justify-center bg-black p-12 ">
       {/* top */}
-      <div className="grid grid-cols-6 grid-rows-2 gap-22">
+      <div className="grid grid-cols-2 md:grid-cols-6 grid-rows-2 h-[40vh] gap-22">
         {/* ------------------------------ */}
         <div className="col-span-2 flex flex-col items-start gap-6 text-white">
           <h1 className="font-bold text-3xl ">Hossiiw</h1>
           <div className="w-full h-[0.5px] bg-gray-400"></div>
-          <p className="w-[70%] text-xl">
+          <p className="w-[100%] md:w-[70%] text-xs md:text-xl">
             1234, Restaurant St, South City New York 0124
           </p>
-          <div className="flex justify-center gap-4">
-            <Link target="_blank" href={"https://github.com/hossiiiw"}>
-              <FaTelegram className="text-2xl text-black bg-white w-16 h-16 rounded-full p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
+          <div className="grid grid-cols-2  md:flex justify-center gap-8 md:gap-4">
+            <Link target="_blank" href={"https://t.me/hossiiiw"}>
+              <FaTelegram className="text-2xl text-black bg-white w-12 h-12 md:w-16 md:h-16 rounded-full p-2 md:p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
             </Link>
             <Link
               target="_blank"
@@ -27,21 +27,21 @@ function Footer() {
                 "https://www.instagram.com/accounts/login/?next=https%3A%2F%2Fwww.instagram.com%2Fhossiiiw%2F&is_from_rle"
               }
             >
-              <FaInstagram className="text-2xl text-black bg-white w-16 h-16 rounded-full p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
+              <FaInstagram className="text-2xl text-black bg-white w-12 h-12 md:w-16 md:h-16 rounded-full p-2 md:p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
             </Link>
             <Link target="_blank" href={"https://github.com/hossiiiw"}>
-              <FaGithub className="text-2xl text-black bg-white w-16 h-16 rounded-full p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
+              <FaGithub className="text-2xl text-black bg-white w-12 h-12 md:w-16 md:h-16 rounded-full p-2 md:p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
             </Link>
             <Link
               target="_blank"
               href={"https://www.linkedin.com/in/hossein-imani-98a40b1a5/"}
             >
-              <FaLinkedin className="text-2xl text-black bg-white w-16 h-16 rounded-full p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
+              <FaLinkedin className="text-2xl text-black bg-white w-12 h-12 md:w-16 md:h-16 rounded-full p-2 md:p-3 hover:bg-orange-400 transition-all duration-300 cursor-pointer " />
             </Link>
           </div>
         </div>
         {/* ------------------------------ */}
-        <div className="col-start-3 flex flex-col gap-3 text-white">
+        <div className="col-start-3  flex flex-col gap-3 text-white ">
           <h2 className="text-xl">LINKS</h2>
           <Link
             href={""}
@@ -75,7 +75,7 @@ function Footer() {
           </Link>
         </div>
         {/* ------------------------------ */}
-        <div className="col-start-4  flex flex-col gap-3 text-white">
+        <div className="col-start-4 hidden md:flex flex-col gap-3 text-white none">
           <h2 className="text-xl">OTHER LINK</h2>
           <Link
             href={""}
@@ -109,13 +109,13 @@ function Footer() {
           </Link>
         </div>
         {/* ------------------------------ */}
-        <div className="col-span-2 col-start-5 flex flex-col gap-3 text-white">
+        <div className="col-span-2 col-start-5 hidden md:flex flex-col gap-3 text-white">
           <h2 className="text-xl">NEWSLETTER</h2>
           <p className="text-lg w-[70%]">
             Subscribe us & receive our offers and updates your inbox directly
           </p>
 
-          <div className="relative w-[70%]">
+          <div className="relative w-[100%] md:w-[100%] lg:w-[70%]">
             <input
               type="text"
               placeholder="Email Address"
@@ -127,13 +127,13 @@ function Footer() {
         {/* ------------------------------ */}
       </div>
 
-      <div className="w-[80%] h-[0.5px] bg-gray-400"></div>
+      <div className="w-[90%] md:w-[80%] h-[0.5px] bg-gray-400"></div>
       {/* top */}
 
       {/* bottom */}
-      <div className="w-[80%] mt-8 flex justify-between items-center text-white">
+      <div className="w-[70%] mt-8 flex-col md:flex justify-between items-center text-white">
         <p>All rights received 2025. Hossiiw Restaurant</p>
-        <div>
+        <div className="hidden md:block">
           <Link href={""}>Terms Of Use</Link>
           <Link href={""}>Privacy Policy</Link>
         </div>
